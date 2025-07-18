933455a0c247ffbb0e2539b95ec285ddc06ec675
sukhsinghntn <sukhsinghntn@gmail.com>
2025-07-18 10:47:51 -0400

    Add feature and feedback email options

--- NdaProcesses.Shared/Services/IEmailService.cs
+++ NdaProcesses.Shared/Services/IEmailService.cs
@@ -6,6 +6,8 @@ namespace DynamicFormsApp.Shared.Services
     public interface IEmailService
     {
         Task SendBugReportEmail(EmailModel email);
+        Task SendFeatureRequestEmail(EmailModel email);
+        Task SendFeedbackEmail(EmailModel email);
         Task SendFormResponseNotification(string toEmail, string formName, int formId);
         Task SendFormShareNotification(string toEmail, string firstName, string formName, string? description, int formId, string sharedBy, string ownerEmail);
         Task SendFormTransferNotification(string toEmail, string formName, string? description, int formId, string transferredBy);
