66472fb4292401ea29f2879fe84929aa9e2777a3
sukhsinghntn <sukhsinghntn@gmail.com>
2025-08-14 10:01:14 -0400

    Enable auto-scroll while dragging

--- NdaProcesses.Shared/Services/IUserService.cs
+++ NdaProcesses.Shared/Services/IUserService.cs
@@ -13,5 +13,7 @@ namespace DynamicFormsApp.Shared.Services
         Task<UserModel> GetUserData(string userName);
         Task<List<UserModel>> GetAllUsers();
         Task<List<UserModel>> SearchUsers(string term);
+        Task<List<string>> GetDepartments();
+        Task<List<string>> GetLocations();
     }
 }
